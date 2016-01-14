f133d34141730171c9aca6d51990736a6987c5e0
Jonathan Ah-Nee <jonahnee@gmail.com>
2016-01-13 21:19:26 -1000

    working on joins #3

---
+++ has_many_blogs.sql
@@ -0,0 +1,39 @@
+-- Drops the users table
+DROP TABLE IF EXISTS users;
+
+-- Creates the users table
+CREATE TABLE users(
+  id serial PRIMARY KEY NOT NULL,
+  username character varying(90) NOT NULL,
+  first_name character varying(90) default NULL,
+  last_name character varying(90) default NULL,
+  created_at timestamp with time zone NOT NULL default now(),
+  updated_at timestamp with time zone NOT NULL default now()
+);
+
+-- Drops the posts table
+DROP TABLE IF EXISTS posts;
+
+-- Creates the posts table
+CREATE TABLE posts(
+  id serial PRIMARY KEY NOT NULL,
+  title character varying(180) default NULL,
+  url character varying(510) default NULL,
+  content text default NULL,
+  created_at timestamp with time zone NOT NULL default now(),
+  updated_at timestamp with time zone NOT NULL default now(),
+  users_id integer REFERENCES users(id)
+);
+
+-- Drops the comments table
+DROP TABLE IF EXISTS comments;
+
+-- Creates the comments table
+CREATE TABLE comments(
+  id serial PRIMARY KEY NOT NULL,
+  body character varying(510) default NULL,
+  created_at timestamp with time zone NOT NULL default now(),
+  updated_at timestamp with time zone NOT NULL default now(),
+  users_id integer REFERENCES users(id),
+  posts_id integer REFERENCES posts(id)
+);
---
+++ joins.sql
@@ -0,0 +1,16 @@
+-- Changes to the has_many_blogs database
+\c has_many_blogs;
+
+-- 1 Query to get all fields from users table
+-- SELECT * from users;
+
+-- 2 Query to get all fields from the posts table where the user_id is 100
+-- SELECT *
+--   FROM posts
+--   WHERE users_id = 100;
+
+-- 3 Query to get all posts fields, user's first and last name
+-- from the posts table where user_id is 200
+SELECT *
+  FROM posts INNER JOIN users ON (posts.users_id = users.users_id)
+  WHERE users_id = 200;
